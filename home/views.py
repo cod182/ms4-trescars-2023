53b3dc25542f3618aa2abc1256f5855acf646d7c
Codie Stephens-Evans <codie18@gmail.com>
2021-12-12 16:36:39 +0000

    navbar initla design

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'home/index.html')
+    template = 'home/index.html'
+    context = {
+        'static': settings.STATIC_URL,
+    }
+    return render(request, template, context)
