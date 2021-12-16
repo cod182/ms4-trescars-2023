c52f1e1c14463a3a2675e433be4fd5d8dc296691
Codie Stephens-Evans <codie18@gmail.com>
2021-12-16 16:19:41 +0000

    added vehicles view

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.conf import settings
+from .models import Vehicle
 
 # Create your views here.
+
+def all_vehicles(request):
+    """
+    A view to show all vehicles, including sorting
+    and search queries
+    """
+    vehicles = Vehicle.objects.all()
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'make':
+                sortkey == 'lower_name'
+                vehicles = vehicles.annotate(lower_name=Lower('make'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            vehicles = vehicles.order_by(sortkey)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter a search term")
+                return redirect(reverse('vehicles'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'vehicles': vehicles,
+        'search_term': query,
+        'current_sorting': current_sorting,
+        'static': settings.STATIC_URL,
+    }
+    return render(request, 'vehicles/vehicles.html', context)
