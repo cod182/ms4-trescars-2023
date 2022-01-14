027297b2f66d9590b26f52ec0c74986598357fa3
Codie Stephens-Evans <codie18@gmail.com>
2022-01-14 21:09:34 +0000

    added a test accessory

--- accessories/views.py
+++ accessories/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.conf import settings
+from .models import Category, Accessory
 
 # Create your views here.
 
 
 def accessories(request):
+
+    accessories = Accessory.objects.all()
+
+    context = {
+        'accessories': accessories,
+        'media': settings.MEDIA_URL
+    }
     template = 'accessories/accessories.html'
 
-    return render(request, template)
+    return render(request, template, context)
