dc35b88034a8dd1dd902f7e746d49dfd3dc2d864
Codie Stephens-Evans <codie18@gmail.com>
2022-01-14 16:40:50 +0000

    added model and admin for accessories

--- accessories/admin.py
+++ accessories/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Accessory, Category
 
-# Register your models here.
+
+class AccessoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'sku',
+        'name',
+        'brand',
+        'vehicle_make',
+        'vehicle_mode',
+        'price',
+        'accessory_type',
+        'quantity_available',
+        'description',
+        'image'
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+
+admin.site.register(Accessory, AccessoryAdmin)
+admin.site.register(Category, CategoryAdmin)
--- accessories/models.py
+++ accessories/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Catagories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Accessory(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=False, blank=False)
+    name = models.CharField(max_length=254, null=False, blank=False)
+    vehicle_make = models.CharField(max_length=50, null=False, blank=False)
+    vehicle_model = models.CharField(max_length=50, null=False, blank=False)
+    brand = models.CharField(max_length=254, null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+    quantity_available = models.IntegerField(null=False, blank=False)
+    accessory_type = models.CharField(max_length=254, null=False, blank=False)
+    type = models.CharField(max_length=254, null=False, blank=False)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
