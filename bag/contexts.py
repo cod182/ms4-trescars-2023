352819c745eb0e1d01c35177fb73fb423594ff5b
Codie Stephens-Evans <codie18@gmail.com>
2022-01-17 11:14:24 +0000

    added accessories to bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from vehicles.models import Vehicle
+from accessories.models import Accessory
 
 
 def vehicle_bag_contents(request):
@@ -41,6 +42,46 @@ def vehicle_bag_contents(request):
         'vehicle_total': total,
         'vehicle_item_count': item_count,
         'vehicle_grand_total': grand_total,
+    }
+
+    return context
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    accessory_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            accessory = get_object_or_404(Accessory, pk=item_id)
+            total += item_data * accessory.price
+            accessory_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'accessory': accessory,
+            })
+        else:
+            accessory = get_object_or_404(Accessory(), pk=item_id)
+            for quantity in item_data.items():
+                total += quantity * accessory.price
+                accessory_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'accessory': accessory,
+                })
+
+    grand_total = total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'accessory_count': accessory_count,
+        'grand_total': grand_total,
     }
 
     return context
--- tres_cars/settings.py
+++ tres_cars/settings.py
@@ -89,6 +89,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'bag.contexts.vehicle_bag_contents',
+                'bag.contexts.bag_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
