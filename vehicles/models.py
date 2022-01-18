32b40c6c50bdb159d46c9a5f97bc096aa7ffd602
Codie Stephens-Evans <codie18@gmail.com>
2022-01-18 09:18:37 +0000

    moved unique_vehicle_parameter to view of vehicles

--- vehicles/models.py
+++ vehicles/models.py
@@ -3,15 +3,6 @@ from django.conf import settings
 from operator import attrgetter
 
 
-class VehicleImages(models.Model):
-    class Meta:
-        verbose_name_plural = 'Vehicle Images'
-
-    name = models.CharField(max_length=254, null=False, blank=False)
-    vehicle_name = models.ForeignKey(
-        'Vehicle', null=True, blank=True, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True)
-    main = models.BooleanField(null=False, blank=False, default=False)
 
 
 gearbox_choices = (
@@ -102,138 +93,12 @@ class Vehicle(models.Model):
         return self.name
 
 
-class unique_vehicle_parameters():
+class VehicleImages(models.Model):
+    class Meta:
+        verbose_name_plural = 'Vehicle Images'
 
-    def unique_vehicle_makes():
-        """
-        Gets all the unique vehicle makes
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_makes = []
-        vehicle_makes.clear()
-
-        for vehicle in vehicles:
-            if vehicle.make not in vehicle_makes:
-                vehicle_makes.append(vehicle.make)
-
-        return vehicle_makes
-
-    def unique_vehicle_models():
-        """
-        Gets the unique vehicle models with make
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_models = []
-        vehicle_models.clear()
-
-        all_values = [value for elem in vehicle_models for value in elem.values()]
-
-        for vehicle in vehicles:
-
-            all_values = [value for elem in vehicle_models for value in elem.values()]
-            if vehicle.model not in all_values:
-                vehicle_models.append({
-                    'make': vehicle.make,
-                    'model': vehicle.model
-                    },
-                )
-
-        return vehicle_models
-
-    def unique_vehicle_colours():
-        """
-        Gets all the unique vehicle colours
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_colours = []
-        vehicle_colours.clear()
-
-        for vehicle in vehicles:
-            if vehicle.colour not in vehicle_colours:
-                vehicle_colours.append(vehicle.colour)
-
-        return sorted(vehicle_colours)
-
-    def unique_vehicle_engines():
-        """
-        Gets all the unique vehicle engines
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_engines = []
-        vehicle_engines.clear()
-
-        for vehicle in vehicles:
-            if vehicle.engine_size not in vehicle_engines:
-                vehicle_engines.append(vehicle.engine_size)
-
-        return sorted(vehicle_engines)
-
-    def unique_vehicle_doors():
-        """
-        Gets all the unique vehicle doors
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_doors = []
-        vehicle_doors.clear()
-
-        for vehicle in vehicles:
-            if vehicle.doors not in vehicle_doors:
-                vehicle_doors.append(vehicle.doors)
-
-        return sorted(vehicle_doors)
-
-    def unique_vehicle_body():
-        """
-        Gets all the unique vehicle body
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_body = []
-        vehicle_body.clear()
-
-        for vehicle in vehicles:
-            if vehicle.body_type not in vehicle_body:
-                vehicle_body.append(vehicle.body_type)
-
-        return sorted(vehicle_body)
-
-    def unique_vehicle_fuels():
-        """
-        Gets all the unique vehicle fuels
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_fuels = []
-        vehicle_fuels.clear()
-
-        for vehicle in vehicles:
-            if vehicle.fuel not in vehicle_fuels:
-                vehicle_fuels.append(vehicle.fuel)
-
-        return sorted(vehicle_fuels)
-
-    def unique_vehicle_drivetrains():
-        """
-        Gets all the unique vehicle drivetrains
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_drivetrains = []
-        vehicle_drivetrains.clear()
-
-        for vehicle in vehicles:
-            if vehicle.drivetrain not in vehicle_drivetrains:
-                vehicle_drivetrains.append(vehicle.drivetrain)
-
-        return sorted(vehicle_drivetrains)
-
-    def unique_vehicle_years():
-        """
-        Gets all the unique vehicle year
-        """
-        vehicles = Vehicle.objects.filter(available='yes')
-        vehicle_years = []
-        vehicle_years.clear()
-
-        for vehicle in vehicles:
-            if vehicle.model_year not in vehicle_years:
-                vehicle_years.append(vehicle.model_year)
-
-        return sorted(vehicle_years)
+    name = models.CharField(max_length=254, null=False, blank=False)
+    vehicle_name = models.ForeignKey(
+        'Vehicle', null=True, blank=True, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    main = models.BooleanField(null=False, blank=False, default=False)
--- vehicles/views.py
+++ vehicles/views.py
@@ -2,11 +2,149 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 from django.core.paginator import Paginator
-from .models import Vehicle, VehicleImages, unique_vehicle_parameters
+from .models import Vehicle, VehicleImages
 from django.db.models import Q
 from decimal import Decimal
 import requests
 import json
+
+
+class unique_vehicle_parameters():
+
+    def unique_vehicle_makes():
+        """
+        Gets all the unique vehicle makes
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_makes = []
+        vehicle_makes.clear()
+
+        for vehicle in vehicles:
+            if vehicle.make not in vehicle_makes:
+                vehicle_makes.append(vehicle.make)
+
+        return vehicle_makes
+
+    def unique_vehicle_models():
+        """
+        Gets the unique vehicle models with make
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_models = []
+        vehicle_models.clear()
+
+        all_values = [value for elem in vehicle_models for value in elem.values()]
+
+        for vehicle in vehicles:
+
+            all_values = [value for elem in vehicle_models for value in elem.values()]
+            if vehicle.model not in all_values:
+                vehicle_models.append({
+                    'make': vehicle.make,
+                    'model': vehicle.model
+                    },
+                )
+
+        return vehicle_models
+
+    def unique_vehicle_colours():
+        """
+        Gets all the unique vehicle colours
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_colours = []
+        vehicle_colours.clear()
+
+        for vehicle in vehicles:
+            if vehicle.colour not in vehicle_colours:
+                vehicle_colours.append(vehicle.colour)
+
+        return sorted(vehicle_colours)
+
+    def unique_vehicle_engines():
+        """
+        Gets all the unique vehicle engines
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_engines = []
+        vehicle_engines.clear()
+
+        for vehicle in vehicles:
+            if vehicle.engine_size not in vehicle_engines:
+                vehicle_engines.append(vehicle.engine_size)
+
+        return sorted(vehicle_engines)
+
+    def unique_vehicle_doors():
+        """
+        Gets all the unique vehicle doors
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_doors = []
+        vehicle_doors.clear()
+
+        for vehicle in vehicles:
+            if vehicle.doors not in vehicle_doors:
+                vehicle_doors.append(vehicle.doors)
+
+        return sorted(vehicle_doors)
+
+    def unique_vehicle_body():
+        """
+        Gets all the unique vehicle body
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_body = []
+        vehicle_body.clear()
+
+        for vehicle in vehicles:
+            if vehicle.body_type not in vehicle_body:
+                vehicle_body.append(vehicle.body_type)
+
+        return sorted(vehicle_body)
+
+    def unique_vehicle_fuels():
+        """
+        Gets all the unique vehicle fuels
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_fuels = []
+        vehicle_fuels.clear()
+
+        for vehicle in vehicles:
+            if vehicle.fuel not in vehicle_fuels:
+                vehicle_fuels.append(vehicle.fuel)
+
+        return sorted(vehicle_fuels)
+
+    def unique_vehicle_drivetrains():
+        """
+        Gets all the unique vehicle drivetrains
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_drivetrains = []
+        vehicle_drivetrains.clear()
+
+        for vehicle in vehicles:
+            if vehicle.drivetrain not in vehicle_drivetrains:
+                vehicle_drivetrains.append(vehicle.drivetrain)
+
+        return sorted(vehicle_drivetrains)
+
+    def unique_vehicle_years():
+        """
+        Gets all the unique vehicle year
+        """
+        vehicles = Vehicle.objects.filter(available='yes')
+        vehicle_years = []
+        vehicle_years.clear()
+
+        for vehicle in vehicles:
+            if vehicle.model_year not in vehicle_years:
+                vehicle_years.append(vehicle.model_year)
+
+        return sorted(vehicle_years)
+
 
 
 def vehicle_search(request):
@@ -196,3 +334,4 @@ def vehicle_detail(request, vehicle_sku):
         'media': settings.MEDIA_URL,
     }
     return render(request, template, context)
+
