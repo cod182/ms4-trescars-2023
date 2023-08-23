c27bd5f410094b04adcacdea142654c0292b0bd9
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-08-23 22:51:26 +0100

    updates for render

--- Procfile
+++
@@ -1,3 +0,0 @@
-worker: python3 manage.py makemigrations
-worker: python3 manage.py migrate
-web: gunicorn tres_cars.wsgi:application
--- tres_cars/settings.py
+++ tres_cars/settings.py
@@ -13,6 +13,11 @@ DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["tres-cars.herokuapp.com", "localhost"]
 
+ # Add Render.com URL to allowed hosts
+ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-cod182-milestoneprojec-0asf9e7g7s0.ws-eu29.gitpod.io",
     "https://tres-cars.herokuapp.com",
