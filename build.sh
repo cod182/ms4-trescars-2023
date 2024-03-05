55d1526b6fc43cf5ab618ed1eee017ecadec4819
Codie Stephens-Evans <codie18@gmail.com>
2024-03-05 11:36:37 +0000

    updating django

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
  set -o errexit
  pip3 install --upgrade pip
+ pip3 install Django===4.0
  pip3 install -r requirements.txt
  python manage.py collectstatic --noinput
  python manage.py makemigrations && python manage.py migrate
